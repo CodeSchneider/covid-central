1efd628fd681056ae799c59f66b5cf086656fdaa
Trey Schneider <trey@codeschneider.com>
2020-08-12 14:08:30 -0400

    add faq

--- frontend/src/components/Novid.jsx
+++ frontend/src/components/Novid.jsx
@@ -48,6 +48,116 @@ export default class Novid extends Component {
             <p>Use the <a href="https://novid.org">NOVID</a> app to anonymously receive alerts about your potential exposure to COVID.  The app does not capture any personally identifiable information.  Full details on NOVID’s security, privacy, and use of permissions can be found <Link to="/privacy-evaluation">here</Link>.</p>
             <p>When you have installed the app, go to Settings and enter the <span>community code BUZZ to join the GT Community</span>.</p>
           </div>
+          <div className="faq">
+            <div className="header">Frequently Asked Questions</div>
+            <div className="questions">
+              <div className="question">
+                <div className="question-header">
+                What is NOVID?
+                </div>
+                <div className="question-content">
+                NOVID is an exposure notification app that is designed to anonymously notify users if they have been in close proximity to individuals who have received a laboratory-confirmed diagnosis of Covid-19.
+                </div>
+              </div>
+              <div className="question">
+                <div className="question-header">
+                Why should I use NOVID?
+                </div>
+                <div className="question-content">
+                Using NOVID is beneficial to both you as an individual and to the broader GT community. As diagnoses of Covid-19 occur on campus, general information about cases will be posted at <a href="http://health.gatech.edu/coronavirus/health-alerts">health.gatech.edu/coronavirus/health-alerts</a>.  But how will you know if you have been personally exposed? One mechanism is through manual contact tracing, where public health officials interview the Covid-positive individual and call their known recent contacts.  But what about interactions they are not aware of?  For example, individuals who were working nearby in a maker space, exercising nearby at the gym?  NOVID can help identify these interactions and inform potentially exposed individuals.  At the same time, NOVID helps individuals who are Covid-19 positive protect their friends, family, and community members in an anonymous way.  Early notifications will help halt the spread of infection and reduce the impact of Covid on our campus.
+                </div>
+              </div>
+              <div className="question">
+                <div className="question-header">
+                How does NOVID detect exposures?
+                </div>
+                <div className="question-content">
+                NOVID uses a combination of bluetooth and high-frequency sound waves to detect distance between two phones running the NOVID app.  An at-risk exposure is an interaction lasting more than 15 minutes within a 6-foot radius.
+                </div>
+              </div>
+              <div className="question">
+                <div className="question-header">
+                How does NOVID record these exposures?
+                </div>
+                <div className="question-content">
+                The NOVID app generates a rotating synthetic code that is shared between phones when there is an interaction.  No identifiable information is captured or shared by the app (see more details below).  The interactions between these synthetic identifiers are stored on your phone and the NOVID network.
+                </div>
+              </div>
+              <div className="question">
+                <div className="question-header">
+                Is NOVID accurate in capturing exposures?
+                </div>
+                <div className="question-content">
+                See detailed performance metrics available on <a href="https://novid.org">novid.org</a> and in <a href="https://www.novid.org/downloads/20200625-accuracy.pdf">this study</a>. Because of iOS restrictions on running apps in the background, use of NOVID's low-power Standby Mode will significantly improve interaction detection involving iOS phones.  For iOS users, we recommend activating and leaving on NOVID Standby Mode whenever you enter a building, class, or gathering.
+                </div>
+              </div>
+              <div className="question">
+                <div className="question-header">
+                What happens if I get notified about a positive exposure?
+                </div>
+                <div className="question-content">
+                If you are notified of a positive exposure (meaning >15 minutes time spent within 6 feet of an individual who has been diagnosed as lab positive since that interaction), you will receive a recommendation to quarantine, monitor for symptoms, and get tested.  If you require class or housing waivers as a result of this notification, you will be provided a link to begin this process.
+                </div>
+              </div>
+              <div className="question">
+                <div className="question-header">
+                How do I notify others anonymously if I am diagnosed positive for Covid-19?
+                </div>
+                <div className="question-content">
+                If you get a positive test result at Stamps Health Center or via <a href="https://mytest.gatech.edu">mytest.gatech.edu</a> testing, you will receive a code that you can enter into the NOVID app to anonymously notify your contacts.
+                </div>
+              </div>
+              <div className="question">
+                <div className="question-header">
+                What data does NOVID capture about me?
+                </div>
+                <div className="question-content">
+                NOVID does not collect any personally identifiable information.  For details on what is captured and how it is used, see <a href="https://covid-central.gatech.edu/app/privacy-evaluation">this report</a> for details of a privacy and security code review conducted independently by Georgia Tech.
+                </div>
+              </div>
+              <div className="question">
+                <div className="question-header">
+                Why does NOVID require access to my device's microphone?
+                </div>
+                <div className="question-content">
+                NOVID uses the device microphone to emit sound which is used to reduce the false-positive rate of Bluetooth signals. GTRI found no evidence that NOVID records audio except as necessary for this purpose. Additionally, GTRI found no evidence that sound collected via the microphone is stored or transmitted.
+                </div>
+              </div>
+              <div className="question">
+                <div className="question-header">
+                Why does the Android version of NOVID require access to my location (GPS)?
+                </div>
+                <div className="question-content">
+                Google devices require that location permissions be granted by the user in order for NOVID to use the Bluetooth functionality. GTRI found no evidence that NOVID uses these permissions to record, track, or otherwise obtain the user's location.
+                </div>
+              </div>
+              <div className="question">
+                <div className="question-header">
+                Does NOVID send my personal information to its servers?
+                </div>
+                <div className="question-content">
+                GTRI observed that data sent to NOVID's backend servers did not contain any user personally identifiable information (PII), which is consistent with their claims. While GTRI did observe that some device information was transmitted, such as the user device brand, model, OS version, and NOVID version, none of this information is specific to an individual NOVID user.
+                </div>
+              </div>
+              <div className="question">
+                <div className="question-header">
+                Will NOVID drain my battery?
+                </div>
+                <div className="question-content">
+                Because NOVID sends out signals only intermittently, it has minimal effect on battery life.  Battery usage is slightly higher on Android than iOS because of Android's background mode.
+                </div>
+              </div>
+              <div className="question">
+                <div className="question-header">
+                Should I activate NOVID's WiFi Background Mode?
+                </div>
+                <div className="question-content">
+                The iOS version of NOVID has an experimental WiFi background mode which was released after GT's code review, so our privacy findings may not apply to this function. Thus we do not currently recommend activating this feature.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="contact-us">Have feedback on NOVID at GT?  Contact us at <u>novid@gtri.gatech.edu</u>.</div>
         </div>
       </>
     );
