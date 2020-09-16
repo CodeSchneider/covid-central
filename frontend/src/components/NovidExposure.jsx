ba524959bc2221146e90cb9c96ddf705d692dffe
Jon Duke <jduke99@gmail.com>
2020-09-16 17:35:51 -0400

    fixed exposures page

--- frontend/src/components/NovidExposure.jsx
+++ frontend/src/components/NovidExposure.jsx
@@ -51,7 +51,7 @@ export default class Novid extends Component {
                 What does a NOVID alert mean?
                 </div>
                 <div className="question-content">
-                A NOVID alert means that your phone has been logged as being in proximity (<3m) for a prolonged period of time (>15 minutes) to a phone whose user has been diagnosed with lab confirmed COVID-19 and whose symptoms began within 48 hours of your contact with them.  If they had no symptoms, all contacts in the 10 days prior to the positive lab test will be notified.
+                A NOVID alert means that your phone has been logged as being in proximity for a prolonged period of time (greater than 15 minutes) to a phone whose user has been diagnosed with lab confirmed COVID-19 and whose symptoms began within 48 hours of your contact with them.  If they had no symptoms, all contacts in the 10 days prior to the positive lab test will be notified.
                 </div>
               </div>
               <div className="question">
@@ -83,7 +83,7 @@ export default class Novid extends Component {
                 Do I need to quarantine?
                 </div>
                 <div className="question-content">
-                YES.  You are at risk of spreading COVID-19 even if asymptomatic.  As a result, you should remain quarantined for
+                YES.  You are at risk of spreading COVID-19 even if asymptomatic.  You should contact Stamps Health Center at (404) 894-1420 and ask to contact to the contact tracing
                  available on <a href="https://novid.org">novid.org</a> and in <a href="https://www.novid.org/downloads/20200625-accuracy.pdf">this study</a>. Because of iOS restrictions on running apps in the background, use of NOVID's low-power Standby Mode will significantly improve interaction detection involving iOS phones. For iOS users, we recommend activating and leaving on NOVID Standby Mode whenever you enter a building, class, or gathering.
                 </div>
               </div>
